43aa424d3c44560c653961d60eb74b8d47c302de
Ronaldinho Ortega Ruiz <112998261+Ronaldinho-OrtegaRuiz@users.noreply.github.com>
2024-01-06 10:58:29 -0500

    Clase DTO

--- src/main/java/com/ronal/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
+++ src/main/java/com/ronal/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
@@ -1,19 +1,38 @@
 package com.ronal.curso.springboot.webapp.springbootweb.controllers;
 
 import com.ronal.curso.springboot.webapp.springbootweb.models.User;
+import com.ronal.curso.springboot.webapp.springbootweb.models.dto.UserDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("api")
 public class UserRestController {
 
     @GetMapping("/details")
-    public Map<String, Object> detail(){
+    public UserDto details() {
+        User user = new User("Ronal", "Ortega");
+        UserDto userDto = new UserDto();
+        userDto.setTitle("User Details");
+        userDto.setUser(user);
+        return userDto;
+    }
+
+    @GetMapping("/list")
+    public List<User> List() {
+        User user = new User("Ronal", "Ortega");
+        User user2 = new User("Ronal", "Ortega");
+        User user3 = new User("Ronal", "Ortega");
+
+        List<User> users = Arrays.asList(user, user2, user3);
+        return users;
+    }
+
+    @GetMapping("/detailsMap")
+    public Map<String, Object> detailMap() {
         User user = new User("Ronal", "Ortega");
         Map<String, Object> response = new HashMap<>();
         response.put("title", "User Details");
---
+++ src/main/java/com/ronal/curso/springboot/webapp/springbootweb/models/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.ronal.curso.springboot.webapp.springbootweb.models.dto;
+
+import com.ronal.curso.springboot.webapp.springbootweb.models.User;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+    private String title;
+    private User user;
+
+}
