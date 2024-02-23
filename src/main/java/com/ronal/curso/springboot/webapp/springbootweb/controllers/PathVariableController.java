94e97f227e548e3f57c1d38e14369154a391bff2
Ronaldinho-OrtegaRuiz <rortegar01@libertadores.edu.co>
2024-02-23 00:46:30 -0500

    Anotación @PathVariable

---
+++ src/main/java/com/ronal/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -0,0 +1,30 @@
+package com.ronal.curso.springboot.webapp.springbootweb.controllers;
+
+import com.ronal.curso.springboot.webapp.springbootweb.models.dto.ParamDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/var")
+public class PathVariableController {
+    @GetMapping("/baz/{message}")
+    public ParamDto baz(@PathVariable() String message){
+        ParamDto param = new ParamDto();
+        param.setMessage(message);
+        return param;
+    }
+
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String,Object> mixPathVar(@PathVariable String product, @PathVariable Long id){
+    Map<String,Object> json = new HashMap<>();
+    json.put("product",product);
+    json.put("id",id);
+    return json;
+    }
+
+}
