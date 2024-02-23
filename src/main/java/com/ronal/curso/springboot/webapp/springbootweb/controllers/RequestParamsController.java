7cbe951929738105981f616e49869e650a173d52
Ronaldinho-OrtegaRuiz <rortegar01@libertadores.edu.co>
2024-02-23 00:30:35 -0500

    Objeto request desde HttpServletRequest

--- src/main/java/com/ronal/curso/springboot/webapp/springbootweb/controllers/RequestParamsController.java
+++ src/main/java/com/ronal/curso/springboot/webapp/springbootweb/controllers/RequestParamsController.java
@@ -1,6 +1,8 @@
 package com.ronal.curso.springboot.webapp.springbootweb.controllers;
 
 import com.ronal.curso.springboot.webapp.springbootweb.models.dto.ParamDto;
+import com.ronal.curso.springboot.webapp.springbootweb.models.dto.ParamMixDto;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,18 +14,32 @@ public class RequestParamsController {
 
     @GetMapping("foo")
     public ParamDto foo(@RequestParam(required = false,
-           defaultValue = "Hello, what are you doing?") String message){
+            defaultValue = "Hello, what are you doing?") String message) {
         ParamDto param = new ParamDto();
         param.setMessage(message);
         return param;
     }
 
     @GetMapping("bar")
-    public ParamDto bar(@RequestParam(required = false,defaultValue = "You wrote nothing") String text,
-                        @RequestParam(required = false, defaultValue = "404") Integer code){
-    ParamDto params = new ParamDto();
-    params.setMessage(text);
-    params.setCode(code);
-    return params;
+    public ParamDto bar(@RequestParam(required = false, defaultValue = "You wrote nothing") String text,
+                        @RequestParam(required = false, defaultValue = "404") Integer code) {
+        ParamDto params = new ParamDto();
+        params.setMessage(text);
+        params.setCode(code);
+        return params;
+    }
+
+    @GetMapping("/request")
+    public ParamMixDto request(HttpServletRequest request) {
+        Integer code = 0;
+        try {
+            code = Integer.parseInt(request.getParameter("code"));
+        } catch (NumberFormatException e) {
+            code = 404;
+        }
+        ParamMixDto params = new ParamMixDto();
+        params.setCode(code);
+        params.setMessage(request.getParameter("message"));
+        return params;
     }
 }
---
+++ src/main/java/com/ronal/curso/springboot/webapp/springbootweb/models/dto/ParamMixDto.java
@@ -0,0 +1,11 @@
+package com.ronal.curso.springboot.webapp.springbootweb.models.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ParamMixDto {
+    private String message;
+    private Integer code;
+}
